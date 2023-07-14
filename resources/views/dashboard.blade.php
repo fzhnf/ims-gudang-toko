4529950ccad02403bb72aaaba3d26833fd7ac0d4
rvyhvn <hanshitrash00@gmail.com>
2023-07-14 21:53:16 +0800

    add: filter hover component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Daftar Produk') }}
+            @include('components.search-bar')
         </h2>
     </x-slot>
 
