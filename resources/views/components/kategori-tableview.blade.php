2ef69aa83ef1a638ac61d53aa04b0ca3de9de04a
sacraBlast <rafialayyubi08@gmail.com>
2023-07-15 23:08:23 +0800

    fixing id table view

--- resources/views/components/kategori-tableview.blade.php
+++ resources/views/components/kategori-tableview.blade.php
@@ -51,7 +51,7 @@
         @foreach ($kategori as $item )
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td class="w-4 p-4">
-                    {{$loop->iteration}}
+                    {{$item->id}}
                 </td>
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {{$item->nama_kategori}}
