70c377f6299e96ed9c64089b9c4441d53d5b0656
sacraBlast <rafialayyubi08@gmail.com>
2023-07-20 13:46:19 +0800

    Add Pemasok model

---
+++ app/Http/Controllers/PemasokController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pemasok;
+use App\Http\Requests\StorePemasokRequest;
+use App\Http\Requests\UpdatePemasokRequest;
+
+class PemasokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePemasokRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pemasok $pemasok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pemasok $pemasok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePemasokRequest $request, Pemasok $pemasok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pemasok $pemasok)
+    {
+        //
+    }
+}
