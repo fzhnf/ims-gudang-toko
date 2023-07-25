8cef18d68489f4f771c6bece08e060fa07f3ef81
fzhnf <faizahnaf52@gmail.com>
2023-07-25 22:19:06 +0800

    test merge

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PemasokController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/kategori/add', function() {
+    return view('kategori.create');
+});
+
+Route::get('/pemasok/add', function() {
+    return view('pemasok.create');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -26,6 +36,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/kategori', KategoriController::class);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/pemasok', PemasokController::class);
 });
 
 require __DIR__.'/auth.php';
