2645eb7b5de62d223a8f356dd2fe6b0b1830e97b
sacraBlast <rafialayyubi08@gmail.com>
2023-07-26 01:42:45 +0800

    Merge files from BE branch

--- database/migrations/2023_07_25_140855_create_produk_table.php
+++ database/migrations/2023_07_25_140855_create_produk_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produks', function (Blueprint $table) {
+        Schema::create('produk', function (Blueprint $table) {
             $table->increments('id_produk');
             $table->unsignedInteger('kategori_id');
             $table->unsignedInteger('pemasok_id');
