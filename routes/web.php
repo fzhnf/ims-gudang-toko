3a861ca0f3f0be1407623375821c4ab1c7667d81
rvyhvn <hanshitrash00@gmail.com>
2023-07-27 23:24:03 +0800

    add(routes): route for dashboard controller connects with produk controller.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\PemasokController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,36 +14,40 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Route::get('/kategori/add', function() {
-    return view('kategori.create');
+Route::get('/kategori/add', function () {
+	return view('kategori.create');
 });
 
-Route::get('/pemasok/add', function() {
-    return view('pemasok.create');
+Route::get('/pemasok/add', function () {
+	return view('pemasok.create');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+	return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route::middleware('auth')->group(function () {
+// 	Route::resource('/dashboard', ProdukController::class);
+// }); gatau ini work atau ga
+
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::resource('/kategori', KategoriController::class);
+	Route::resource('/kategori', KategoriController::class);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::resource('/pemasok', PemasokController::class);
+	Route::resource('/pemasok', PemasokController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
