05bc159de47d48cf7ae5289e95d1d8fbdc38abce
rvyhvn <hanshitrash00@gmail.com>
2023-07-30 09:35:17 +0800

    fix(seeder): not null violations

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -5,31 +5,32 @@ namespace Database\Seeders;
 use App\Models\Produk;
 use Illuminate\Database\Seeder;
 
-class ProdukSeeder extends Seeder {
-	/**
-	 * Run the database seeds.
-	 */
-	public function run(): void {
+class ProdukSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
 
-		\App\Models\Produk::factory(20)->create();
-		// There is no need to set the nama_kategori attribute, you can access it directly from the relationship
-		$produksWithKategoriAndPemasok = Produk::with('kategori', 'pemasok')->get();
+        \App\Models\Produk::factory(20)->create();
+        // There is no need to set the nama_kategori attribute, you can access it directly from the relationship
+        $produksWithKategoriAndPemasok = Produk::with('kategori', 'pemasok')->get();
 
-		// Menampilkan informasi produk beserta informasi kategori
-		// Pindah ke controller iz
-		foreach ($produksWithKategoriAndPemasok as $produk) {
-			$nama_produk = $produk->nama_produk;
-			$quantity = $produk->quantity;
-			$harga_per_pcs = $produk->harga_per_pcs;
-			$nama_kategori = $produk->kategori->nama_kategori;
-			$nama_pemasok = $produk->pemasok->nama_pemasok;
+        // Menampilkan informasi produk beserta informasi kategori
+        // Pindah ke controller iz
+        foreach ($produksWithKategoriAndPemasok as $produk) {
+            $nama_produk = $produk->nama_produk;
+            $quantity = $produk->quantity;
+            $harga_per_pcs = $produk->harga_per_pcs;
+            $nama_kategori = $produk->kategori->nama_kategori;
+            $nama_pemasok = $produk->pemasok->nama_pemasok;
 
-			echo "Nama Produk: " . $nama_produk . ", ";
-			echo "Quantity: " . $quantity . ", ";
-			echo "Harga per pcs: " . $harga_per_pcs . ", ";
-			echo "Nama Kategori: " . $nama_kategori . ", ";
-			echo "Nama Pemasok: " . $nama_pemasok . "\n ";
-		}
-
-	}
+            echo "Nama Produk: " . $nama_produk . ", ";
+            echo "Quantity: " . $quantity . ", ";
+            echo "Harga per pcs: " . $harga_per_pcs . ", ";
+            echo "Nama Kategori: " . $nama_kategori . ", ";
+            echo "Nama Pemasok: " . $nama_pemasok . "\n ";
+        }
+    }
 }
