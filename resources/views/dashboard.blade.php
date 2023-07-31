42dc688d2ba4e09c6e202234df4b1d5225f80830
rvyhvn <hanshitrash00@gmail.com>
2023-08-01 03:39:40 +0800

    feat: add link button to head to produk page on dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("ANJERRRRRRRRR") }}
+                <div class="p-6 text-gray-900 flex justify-center items-center py-6">
+                    <a href="/produk" class="text-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded">Lihat Daftar Produk</a>
+
                 </div>
             </div>
         </div>
