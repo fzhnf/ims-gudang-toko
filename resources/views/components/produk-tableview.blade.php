bf19c15572be7a101afa3e6de40147b6fac87ebf
rvyhvn <hanshitrash00@gmail.com>
2023-08-03 13:22:39 +0800

    feat: sort by for produk. sort by produk name asc/desc, sort by kategori name asc/desc, sort by pemasok name asc/desc.

--- resources/views/components/produk-tableview.blade.php
+++ resources/views/components/produk-tableview.blade.php
@@ -30,16 +30,34 @@
 <div id="dropdownRadio" class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
     <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
         <li>
-            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                <input id="filter-radio-example-1" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="filter-radio-example-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Nama Produk A-Z</label>
-            </div>
+            <a href="{{ route('produk.index', ['sort' => 'nama_produk_az']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Produk (A-Z)
+            </a>
         </li>
         <li>
-            <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                <input checked="" id="filter-radio-example-2" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="filter-radio-example-2" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Nama Produk Z-A</label>
-            </div>
+            <a href="{{ route('produk.index', ['sort' => 'nama_produk_za']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Produk (Z-A)
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('produk.index', ['sort' => 'nama_kategori_az']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Kategori (A-Z)
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('produk.index', ['sort' => 'nama_kategori_za']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Kategori (Z-A)
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('produk.index', ['sort' => 'nama_pemasok_az']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Pemasok (A-Z)
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('produk.index', ['sort' => 'nama_pemasok_za']) }}" class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                Nama Pemasok (Z-A)
+            </a>
         </li>
     </ul>
 </div>
