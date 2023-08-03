4bc8848229af071d42ce995909a5c8722e0b3d6e
rvyhvn <hanshitrash00@gmail.com>
2023-08-03 13:15:04 +0800

    fix: conditioning when there is no kategori or pemasok in produk in the table view

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -1,58 +1,60 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use App\Http\Requests\StoreProdukRequest;
 use App\Http\Requests\UpdateProdukRequest;
 use App\Models\Kategori;
 use App\Models\Pemasok;
 use App\Models\Produk;
-use DB;
-use Symfony\Component\HttpFoundation\Request;
- 
+use Illuminate\Support\Facades\DB;
+
 class ProdukController extends Controller {
-	/**
-	 * Display a listing of the resource.
-	 */
-	public function index(Request $request) {
+	public function index() {
 		$searchProduk = request()->query('search');
+		$sortBy = request()->query('sort');
+
 		$dataProdukQuery = Produk::query();
- 
+
 		if (!empty($searchProduk)) {
-			$dataProdukQuery->where('produks.nama_produk', 'ILIKE', $searchProduk . '%');
+			$dataProdukQuery->where('nama_produk', 'ILIKE', $searchProduk . '%');
 		}
-		$dataProduk = $dataProdukQuery->with('kategori', 'pemasok')
-			->paginate(20)
-			->fragment('prd');
- 
-		return view('produk.index')->with([
+
+		if ($sortBy == 'nama_produk_az') {
+			$dataProdukQuery->orderBy('nama_produk', 'asc');
+		} elseif ($sortBy == 'nama_produk_za') {
+			$dataProdukQuery->orderBy('nama_produk', 'desc');
+		} elseif ($sortBy == 'nama_kategori_az') {
+			$dataProdukQuery->join('kategori', 'produks.kategori_id', '=', 'kategori.id_kategori')
+				->orderBy('kategori.nama_kategori', 'asc');
+		} elseif ($sortBy == 'nama_kategori_za') {
+			$dataProdukQuery->join('kategori', 'produks.kategori_id', '=', 'kategori.id_kategori')
+				->orderBy('kategori.nama_kategori', 'desc');
+		} elseif ($sortBy == 'nama_pemasok_az') {
+			$dataProdukQuery->join('pemasok', 'produks.pemasok_id', '=', 'pemasok.id_pemasok')
+				->orderBy('pemasok.nama_pemasok', 'asc');
+		} elseif ($sortBy == 'nama_pemasok_za') {
+			$dataProdukQuery->join('pemasok', 'produks.pemasok_id', '=', 'pemasok.id_pemasok')
+				->orderBy('pemasok.nama_pemasok', 'desc');
+		}
+
+		$dataProduk = $dataProdukQuery->with('kategori', 'pemasok')->paginate(20);
+
+		return view('produk.index', [
 			'produk' => $dataProduk,
 			'searchProduk' => $searchProduk,
 		]);
 	}
- 
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create() {
-		//
-	}
- 
-	/**
-	 * Store a newly created resource in storage.
-	 */
+
 	public function store(StoreProdukRequest $request) {
 		$validated = $request->validated();
- 
+
 		$kategoriName = ucwords($validated['txtkategori']);
 		$pemasokName = ucwords($validated['txtpemasok']);
-		// Check if the kategori already exists, otherwise create a new one
+
 		$kategori = Kategori::firstOrCreate(['nama_kategori' => $kategoriName]);
- 
-		// Check if the pemasok already exists, otherwise create a new one
 		$pemasok = Pemasok::firstOrCreate(['nama_pemasok' => $pemasokName]);
- 
-		// Create the new Produk instance with the provided data and the IDs of kategori and pemasok
+
 		$produk = new Produk([
 			'nama_produk' => $validated['txtproduk'],
 			'quantity' => $validated['txtkuantitas'],
@@ -60,111 +62,78 @@ class ProdukController extends Controller {
 			'kategori_id' => $kategori->id_kategori,
 			'pemasok_id' => $pemasok->id_pemasok,
 		]);
- 
-		// Save the new Produk to the database
+
 		$produk->save();
- 
+
 		return redirect('produk')->with('msg', 'Produk successfully added');
 	}
-	/**
-	 * Display the specified resource.
-	 */
+
 	public function show($id_produk) {
 		$data = Produk::find($id_produk);
-		return view('produk.edit')->with(
-			[
-				'txtid' => $id_produk,
-				'txtproduk' => $data->nama_produk,
-				'txtkategori' => $data->kategori->nama_kategori,
-				'txtpemasok' => $data->pemasok->nama_pemasok,
-				'txtkuantitas' => $data->quantity,
-				'txthargaperpcs' => $data->harga_per_pcs,
-			]
-		);
+
+		return view('produk.edit', [
+			'txtid' => $id_produk,
+			'txtproduk' => $data->nama_produk,
+			'txtkategori' => optional($data->kategori)->nama_kategori,
+			'txtpemasok' => optional($data->pemasok)->nama_pemasok,
+			'txtkuantitas' => $data->quantity,
+			'txthargaperpcs' => $data->harga_per_pcs,
+		]);
 	}
- 
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit(Produk $produk) {
-		// return view('produk.edit')->with('produk', $produk);
-	}
- 
-	/**
-	 * Update the specified resource in storage.
-	 */
+
 	public function update(UpdateProdukRequest $request, $id_produk) {
 		$validated = $request->validated();
 		$data = Produk::find($id_produk);
- 
+
 		$data->nama_produk = $request->txtproduk;
 		$data->quantity = $request->txtkuantitas;
 		$data->harga_per_pcs = $request->txthargaperpcs;
- 
-		// Simpan ID kategori dan ID pemasok sebelumnya
-		$oldKategoriId = $data->kategori_id;
-		$oldPemasokId = $data->pemasok_id;
- 
+
 		if (!empty($validated['txtkategori'])) {
 			$kategoriName = ucwords($validated['txtkategori']);
 			$kategori = Kategori::updateOrCreate(
-				['id_kategori' => $oldKategoriId],
+				['id_kategori' => optional($data->kategori)->id_kategori],
 				['nama_kategori' => $kategoriName]
 			);
-			$data->kategori()->associate($kategori);
+			$data->kategori_id = $kategori->id_kategori;
 		}
- 
+
 		if (!empty($validated['txtpemasok'])) {
 			$pemasokName = ucwords($validated['txtpemasok']);
 			$pemasok = Pemasok::updateOrCreate(
-				['id_pemasok' => $oldPemasokId],
+				['id_pemasok' => optional($data->pemasok)->id_pemasok],
 				['nama_pemasok' => $pemasokName]
 			);
-			$data->pemasok()->associate($pemasok);
+			$data->pemasok_id = $pemasok->id_pemasok;
 		}
- 
+
 		$data->save();
- 
-		// Update the kategori and pemasok in their respective tables if the association has changed
-		if ($oldKategoriId !== $data->kategori_id) {
-			DB::table('kategori')->where('id_kategori', $oldKategoriId)->update(['nama_kategori' => $kategoriName]);
-		}
- 
-		if ($oldPemasokId !== $data->pemasok_id) {
-			DB::table('pemasok')->where('id_pemasok', $oldPemasokId)->update(['nama_pemasok' => $pemasokName]);
-		}
- 
+
 		return redirect('produk')->with('msg', 'Produk successfully updated');
 	}
-	/**
-	 * Remove the specified resource from storage.
-	 */
+
 	public function destroy($id_produk) {
 		$data = Produk::find($id_produk);
- 
-		// Simpan ID kategori dan ID pemasok sebelumnya
-		$oldKategoriId = $data->kategori_id;
-		$oldPemasokId = $data->pemasok_id;
- 
-		// Hapus relasi dengan pemasok dan kategori (reset to NULL)
+
+		$oldKategoriId = optional($data->kategori)->id_kategori;
+		$oldPemasokId = optional($data->pemasok)->id_pemasok;
+
 		$data->pemasok_id = null;
 		$data->kategori_id = null;
 		$data->save();
- 
-		// Hapus produk dari database
+
 		$data->delete();
- 
-		// Update the kategori and pemasok in their respective tables if the association has changed
+
 		if ($oldKategoriId !== null) {
 			$kategoriName = Kategori::find($oldKategoriId)->nama_kategori;
 			DB::table('kategori')->where('id_kategori', $oldKategoriId)->update(['nama_kategori' => $kategoriName]);
 		}
- 
+
 		if ($oldPemasokId !== null) {
 			$pemasokName = Pemasok::find($oldPemasokId)->nama_pemasok;
 			DB::table('pemasok')->where('id_pemasok', $oldPemasokId)->update(['nama_pemasok' => $pemasokName]);
 		}
- 
+
 		return redirect('produk')->with('msg', 'Produk successfully deleted');
 	}
 }
--- resources/views/produk/edit.blade.php
+++ resources/views/produk/edit.blade.php
@@ -18,12 +18,12 @@
             </div>
             <div class="mb-4">
                 <label for="txtkategori" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Kategori</label>
-                <input type="text" id="txtkategori" name="txtkategori" value="{{ $txtkategori }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <input type="text" id="txtkategori" name="txtkategori" value="{{ $txtkategori !== 'No Category' ? $txtkategori : '' }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
             </div>
             <div class="mb-4">
                 <label for="txtpemasok" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pemasok</label>
-                <input type="text" id="txtpemasok" name="txtpemasok" value="{{ $txtpemasok }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <input type="text" id="txtpemasok" name="txtpemasok" value="{{ $txtpemasok !== 'No Supplier' ? $txtpemasok : ''}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
             </div>
             <div class="mb-6">
